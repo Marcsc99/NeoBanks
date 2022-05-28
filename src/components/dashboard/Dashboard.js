8b812fb4349236bc0b6a6c22679eb195c860a5ff
Marc Sanchez Civantos <marc.sanchez.civantos@gmail.com>
2022-05-28 17:27:17 +0100

    fix mobile navbar

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { StyledBG } from "../styled-components/StyledBG";
 import Header from "../header";
 import DashText from "../dash-text";
 import Container from "../container";
-import MobileNavBar from "../container/mobile-navbar";
+import MobileNavBar from "../mobile-navbar";
 import { types } from "../../deviceHelper/deviceTypeController";
 
 const Dashboard = () => {
